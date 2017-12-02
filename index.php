cf4d49214dabc967abe84025e4a3be0b8c60c2fb
Jalquin <33669466+Jalquin@users.noreply.github.com>
2017-12-02 15:49:00 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <title>Informační systém</title>
@@ -21,7 +16,7 @@ and open the template in the editor.
                 <nav class="col-sm-8 text-right menu">
                     <a href="test.php">TEST</a>
                     <a href="game.php">Hra</a>    
-                    <a href="index.html">Domů</a>
+                    <a href="index.php">Domů</a>
                     <a href="http://skolakrizik.cz">Školní web</a>
                     <a href="https://krizik.bakalari.cz:8880/next/login.aspx">Bakalaři</a>
                 </nav>
@@ -29,20 +24,20 @@ and open the template in the editor.
         </header>
             <div class="row">
                 <div class="col-4">
-                    <a href="http://213.151.86.106/_rozvrh/tridy.pdf">
-                        <img class="img-responsive" alt="" src="https://camo.githubusercontent.com/39bc046bb822914448966a9820c0d793deede43a/687474703a2f2f61313230332e70686f626f732e6170706c652e636f6d2f75732f72313030302f3036332f507572706c652f76342f35362f64662f36612f35366466366136382d366532342d363366312d666364332d6339333334623162306561642f6d7a6c2e61777771707769792e706e67">
+                    <a href="rozvrh.html">
+                        <img class="img-responsive" alt="Rozvrh" src="https://camo.githubusercontent.com/39bc046bb822914448966a9820c0d793deede43a/687474703a2f2f61313230332e70686f626f732e6170706c652e636f6d2f75732f72313030302f3036332f507572706c652f76342f35362f64662f36612f35366466366136382d366532342d363366312d666364332d6339333334623162306561642f6d7a6c2e61777771707769792e706e67">
                         <div class="top-left">Rozvrh</div>
                     </a>       
                 </div>
                 <div class="col-4">
-                    <a href="https://nastenka.skolakrizik.cz/substitution">
-                        <img class="img-responsive" alt="" src="http://www.woodesigner.net/wp-content/uploads/2015/01/business-note-on-bulletin-board.jpg">
+                    <a href="nastenka.html">
+                        <img class="img-responsive" alt="Nástěnka" src="http://www.woodesigner.net/wp-content/uploads/2015/01/business-note-on-bulletin-board.jpg">
                         <div class="top-left">Nástěnka</div>
                     </a>                    
                 </div>
                 <div class="col-4">
                     <a href="">
-                        <img class="img-responsive" alt="" src="https://image.flaticon.com/icons/svg/65/65882.svg">
+                        <img class="img-responsive" alt="Hodnocení profesorů" src="https://image.flaticon.com/icons/svg/65/65882.svg">
                         <div class="top-left">Hodnocení Profesorů</div>
                     </a>                   
                 </div>
@@ -50,19 +45,19 @@ and open the template in the editor.
             <div class="row">
                 <div class="col-4">
                     <a href="">
-                        <img class="img-responsive" alt="" src="https://www.shareicon.net/download/2015/12/27/694080_test.svg">
+                        <img class="img-responsive" alt="Známky" src="https://www.shareicon.net/download/2015/12/27/694080_test.svg">
                         <div class="top-left">Známky</div>
                     </a>                    
                 </div>
                 <div class="col-4">
-                    <a href="http://www.vosaspsekrizik.cz/cs/uvodni-stranka/novinky.ep">
-                        <img class="img-responsive" alt="" src="https://maxcdn.icons8.com/Share/icon/p1em/Very_Basic//news1600.png">
+                    <a href="novinky.html">
+                        <img class="img-responsive" alt="Novinky" src="https://maxcdn.icons8.com/Share/icon/p1em/Very_Basic//news1600.png">
                         <div class="top-left">Novinky</div>
                     </a>                    
                 </div>
                 <div class="col-4">
                     <a href="">
-                        <img class="img-responsive" alt="" src="https://image.flaticon.com/icons/svg/60/60473.svg">
+                        <img class="img-responsive" alt="Nastavení" src="https://image.flaticon.com/icons/svg/60/60473.svg">
                         <div class="top-left">Nastavení</div>
                     </a>                        
                 </div>
@@ -70,19 +65,19 @@ and open the template in the editor.
             <div class="row">
                 <div class="col-4">
                     <a href="https://www.facebook.com/krizici/">
-                        <img class="img-responsive" alt="" src="https://cdn.worldvectorlogo.com/logos/facebook-icon.svg">
+                        <img class="img-responsive" alt="Facebook Školy" src="https://cdn.worldvectorlogo.com/logos/facebook-icon.svg">
                         <div class="top-left">Facebook Školy</div>
                     </a>
                 </div>
                 <div class="col-4">
                     <a href="https://www.facebook.com/">
-                        <img class="img-responsive" alt="" src="https://cdn.worldvectorlogo.com/logos/facebook-icon.svg">
+                        <img class="img-responsive" alt="Facebook Třídy" src="https://cdn.worldvectorlogo.com/logos/facebook-icon.svg">
                         <div class="top-left">Facebook Třídy</div>
                     </a>                    
                 </div>
                 <div class="col-4">
                     <a href="https://mail.google.com/">
-                        <img class="img-responsive" alt="" src="http://workplaceinsight.net/wp-content/uploads/2015/05/24637-email-icon.jpg">
+                        <img class="img-responsive" alt="e-mail" src="http://workplaceinsight.net/wp-content/uploads/2015/05/24637-email-icon.jpg">
                         <div class="top-left">e-mail</div>
                     </a>                    
                 </div>
