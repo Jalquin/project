13822f79016d611d44939f2fda5d7a1bbc5ada4f
Jalquin <33669466+Jalquin@users.noreply.github.com>
2017-12-06 18:29:50 +0100

    Add files via upload

--- game.php
+++ game.php
@@ -4,58 +4,52 @@
 session_start();
 
 if(!isset ($_SESSION['start'])){$_SESSION['start'] = '1';}
-$_SESSION['action'] = rand(0,2);
-$_SESSION['dmg1'] = rand(1,3);
-$_SESSION['dmg2'] = rand(1,3);
-$_SESSION['rest_ammount'] = rand(4,5);
+$action = rand(0,2);
+$dmg1 = rand(1,3);
+$dmg2 = rand(1,3);
+$MSG1 = NULL;
+$MSG2 = NULL;
+$MSG3 = NULL;
 
 /*------------RESET---------------*/
 
 if (isset($_POST["reset"])){
     $_SESSION['start'] = '1';
-    $_SESSION['MSG1'] = NULL;
-    $_SESSION['MSG2'] = NULL;
-    $_SESSION['MSG3'] = NULL;
 }
-
 /*------------DEKLARACE PROMĚNNÝCH---------------*/
 
-if($_SESSION['start'] == 1){
+if($_SESSION['start'] == 1) {
     $_SESSION['hp1'] = '10';
     $_SESSION['hp2'] = '10';
     $_SESSION['sta1'] = '10';
     $_SESSION['sta2'] = '10';
-    $_SESSION['arm1'] = '4';
-    $_SESSION['arm2'] = '4';
-    $_SESSION['MSG1'] = NULL;
-    $_SESSION['MSG2'] = NULL;
-    $_SESSION['MSG3'] = NULL;
- }
-
+    $_SESSION['arm1'] = '5';
+    $_SESSION['arm2'] = '5';
+}
  /*------------FUNKCE TLAČÍTEK---------------*/
   
  /*------------TLAČÍTKO ÚTOK---------------*/
   
 if (isset($_POST["attack"])){
           if($_SESSION['sta1'] >= 2){ $_SESSION['sta1'] = $_SESSION['sta1'] - 2;
-          if ($_SESSION['dmg1'] > $_SESSION['arm2']){$_SESSION['hp2'] = $_SESSION['hp2'] - ($_SESSION['dmg1']-$_SESSION['arm2']);}
-          if($_SESSION['arm2'] > 0){ $_SESSION['arm2'] = $_SESSION['arm2'] - $_SESSION['dmg1']; }
-          else {$_SESSION['hp2'] = $_SESSION['hp2'] - $_SESSION['dmg1'];}
+          if ($dmg1 > $_SESSION['arm2']){$_SESSION['hp2'] = $_SESSION['hp2'] - ($dmg1-$_SESSION['arm2']);}
+          if($_SESSION['arm2'] > 0){ $_SESSION['arm2'] = $_SESSION['arm2'] - $dmg1; }
+          else {$_SESSION['hp2'] = $_SESSION['hp2'] - $dmg1;}
           }
           else{$_SESSION['sta1'] = $_SESSION['sta1'] + 4;}
           
-          switch ($_SESSION['action']) {
+          switch ($action) {
               case 0: //ATTACK
                   if($_SESSION['sta2'] >= 2){ $_SESSION['sta2'] = $_SESSION['sta2'] - 2;
-                  if ($_SESSION['dmg2'] > $_SESSION['arm1']){$_SESSION['hp1'] = $_SESSION['hp1'] - ($_SESSION['dmg2']-$_SESSION['arm1']);}
-                  if($_SESSION['arm1'] > 0){ $_SESSION['arm1'] = $_SESSION['arm1'] - $_SESSION['dmg2']; }
-                  else {$_SESSION['hp1'] = $_SESSION['hp1'] - $_SESSION['dmg2'];}
+                  if ($dmg2 > $_SESSION['arm1']){$_SESSION['hp1'] = $_SESSION['hp1'] - ($dmg2-$_SESSION['arm1']);}
+                  if($_SESSION['arm1'] > 0){ $_SESSION['arm1'] = $_SESSION['arm1'] - $dmg2; }
+                  else {$_SESSION['hp1'] = $_SESSION['hp1'] - $dmg2;}
                   }
                   else{$_SESSION['sta2'] = $_SESSION['sta2'] + 4;}
                   break;
               case 1: //BLOCK
                   if($_SESSION['sta2'] >= 1){ $_SESSION['sta2'] = $_SESSION['sta2'] - 1;
-                  if(isset($_POST["attack"])){$_SESSION['arm2'] = $_SESSION['arm2'] + ($_SESSION['dmg1'] / 2);}
+                  if(isset($_POST["attack"])){$_SESSION['arm2'] = $_SESSION['arm2'] + ($dmg1 / 2);}
                   else{ $_SESSION['arm2'] = $_SESSION['arm2'] + 1; }
                   if ($_SESSION['arm2'] > 5){$_SESSION['arm2'] = 5;}
                   }
@@ -66,35 +60,32 @@ if (isset($_POST["attack"])){
                   if ($_SESSION['sta2'] > 10){$_SESSION['sta2'] = 10;}
                   break;
 }
-          if ($_SESSION['action'] == 0){$_SESSION['action'] = 'Útok';} elseif($_SESSION['action'] == 1){$_SESSION['action'] = 'Obranu';} else {$_SESSION['action'] = 'Odpočinek';}
+          if ($action == 0){$action = 'Útok';} elseif($action == 1){$action = 'Obranu';} else {$action = 'Odpočinek';}
           $_SESSION['start'] = 0;
-          $_SESSION['MSG1'] = '<br />Zmáčknul jsi tlačítko ' . $_POST['attack'] . ', zaútočil jsi za ' . $_SESSION['dmg1'] . ' bodů poškození.<br />' . '<br />PC použil ' . $_SESSION['action'] . '.<br />';
-          $_SESSION['MSG2'] = NULL;
-          $_SESSION['MSG3'] = NULL;
+          $MSG1 = '<br />Zmáčknul jsi tlačítko ' . $_POST['attack'] . ', zaútočil jsi za ' . $dmg1 . ' bodů poškození.<br />' . '<br />PC použil ' . $action . '.<br />';
 }
-
  /*------------TLAČÍTKO OBRANA---------------*/                                                                                                               
 
 if (isset($_POST["block"])){
           if($_SESSION['sta1'] >= 1){ $_SESSION['sta1'] = $_SESSION['sta1'] - 1;
-          if($_SESSION['action'] == 1){$_SESSION['arm1'] = $_SESSION['arm1'] + ($_SESSION['dmg2']/2);}
+          if($action == 1){$_SESSION['arm1'] = $_SESSION['arm1'] + ($dmg2/2);}
           else{ $_SESSION['arm1'] = $_SESSION['arm1'] + 1; }
           if ($_SESSION['arm1'] > 5){$_SESSION['arm1'] = 5;}
           }
           else{$_SESSION['sta1'] = $_SESSION['sta1'] + 4;}
           
-           switch ($_SESSION['action']) {
+           switch ($action) {
               case 0: //ATTACK
                   if($_SESSION['sta2'] >= 2){ $_SESSION['sta2'] = $_SESSION['sta2'] - 2;
-                  if ($_SESSION['dmg2'] > $_SESSION['arm1']){$_SESSION['hp1'] = $_SESSION['hp1'] - ($_SESSION['dmg2']-$_SESSION['arm1']);}
-                  if($_SESSION['arm1'] > 0){ $_SESSION['arm1'] = $_SESSION['arm1'] - $_SESSION['dmg2']; }
-                  else {$_SESSION['hp1'] = $_SESSION['hp1'] - $_SESSION['dmg2'];}
+                  if ($dmg2 > $_SESSION['arm1']){$_SESSION['hp1'] = $_SESSION['hp1'] - ($dmg2-$_SESSION['arm1']);}
+                  if($_SESSION['arm1'] > 0){ $_SESSION['arm1'] = $_SESSION['arm1'] - $dmg2; }
+                  else {$_SESSION['hp1'] = $_SESSION['hp1'] - $dmg2;}
                   }
                   else{$_SESSION['sta2'] = $_SESSION['sta2'] + 4;}
                   break;
               case 1: //BLOCK
                   if($_SESSION['sta2'] >= 1){ $_SESSION['sta2'] = $_SESSION['sta2'] - 1;
-                  if(isset($_POST["attack"])){$_SESSION['arm2'] = $_SESSION['arm2'] + ($_SESSION['dmg1'] / 2);}
+                  if(isset($_POST["attack"])){$_SESSION['arm2'] = $_SESSION['arm2'] + ($dmg1 / 2);}
                   else{ $_SESSION['arm2'] = $_SESSION['arm2'] + 1; }
                   if ($_SESSION['arm2'] > 5){$_SESSION['arm2'] = 5;}
                   }
@@ -105,31 +96,28 @@ if (isset($_POST["block"])){
                   if ($_SESSION['sta2'] > 10){$_SESSION['sta2'] = 10;}
                   break;
 } 
-          if ($_SESSION['action'] == 0){$_SESSION['action'] = 'Útok';} elseif($_SESSION['action'] == 1){$_SESSION['action'] = 'Obranu';} else {$_SESSION['action'] = 'Odpočinek';}
+          if ($action == 0){$action = 'Útok';} elseif($action == 1){$action = 'Obranu';} else {$action = 'Odpočinek';}
           $_SESSION['start'] = 0;
-          $_SESSION['MSG1'] = NULL;
-          $_SESSION['MSG2'] = '<br />Zmáčknul jsi tlačítko ' . $_POST['block'] . '<br />PC použil ' . $_SESSION['action'] . '.<br />'; 
-          $_SESSION['MSG3'] = NULL;
+          $MSG2 = '<br />Zmáčknul jsi tlačítko ' . $_POST['block'] . ' <br />' . '<br />PC použil ' . $action . '.<br />';
 }
-
  /*------------TLAČÍTKO ÚTOK---------------*/
 
 if (isset($_POST["rest"])){
           $_SESSION['sta1'] = $_SESSION['sta1'] + 4;
           if ($_SESSION['sta1'] > 10){$_SESSION['sta1'] = 10;}
           
-           switch ($_SESSION['action']) {
+           switch ($action) {
               case 0: //ATTACK
                   if($_SESSION['sta2'] >= 2){ $_SESSION['sta2'] = $_SESSION['sta2'] - 2;
-                  if ($_SESSION['dmg2'] > $_SESSION['arm1']){$_SESSION['hp1'] = $_SESSION['hp1'] - ($_SESSION['dmg2']-$_SESSION['arm1']);}
-                  if($_SESSION['arm1'] > 0){ $_SESSION['arm1'] = $_SESSION['arm1'] - $_SESSION['dmg2']; }
-                  else {$_SESSION['hp1'] = $_SESSION['hp1'] - $_SESSION['dmg2'];}
+                  if ($dmg2 > $_SESSION['arm1']){$_SESSION['hp1'] = $_SESSION['hp1'] - ($dmg2-$_SESSION['arm1']);}
+                  if($_SESSION['arm1'] > 0){ $_SESSION['arm1'] = $_SESSION['arm1'] - $dmg2; }
+                  else {$_SESSION['hp1'] = $_SESSION['hp1'] - $dmg2;}
                   }
                   else{$_SESSION['sta2'] = $_SESSION['sta2'] + 4;}
                   break;
               case 1: //BLOCK
                   if($_SESSION['sta2'] >= 1){ $_SESSION['sta2'] = $_SESSION['sta2'] - 1;
-                  if(isset($_POST["attack"])){$_SESSION['arm2'] = $_SESSION['arm2'] + ($_SESSION['dmg1'] / 2);}
+                  if(isset($_POST["attack"])){$_SESSION['arm2'] = $_SESSION['arm2'] + ($dmg1 / 2);}
                   else{ $_SESSION['arm2'] = $_SESSION['arm2'] + 1; }
                   if ($_SESSION['arm2'] > 5){$_SESSION['arm2'] = 5;}
                   }
@@ -138,17 +126,12 @@ if (isset($_POST["rest"])){
               case 2: //REST
                   $_SESSION['sta2'] = $_SESSION['sta2'] + 4;
                   if ($_SESSION['sta2'] > 10){$_SESSION['sta2'] = 10;}
-                  break;
-}     
-          if ($_SESSION['action'] == 0){$_SESSION['action'] = 'Útok';} elseif($_SESSION['action'] == 1){$_SESSION['action'] = 'Obranu';} else {$_SESSION['action'] = 'Odpočinek';}
+                  break;    }
+          if ($action == 0){$action = 'Útok';} elseif($action == 1){$action = 'Obranu';} else {$action = 'Odpočinek';}
           $_SESSION['start'] = 0;
-          $_SESSION['MSG1'] = NULL;
-          $_SESSION['MSG2'] = NULL;
-          $_SESSION['MSG3'] = '<br />Zmáčknul jsi tlačítko ' . $_POST['rest'] . ', obnovil jsi ' . $_SESSION['rest_ammount'] . ' bodů výdrže.<br />' . '<br />PC použil ' . $_SESSION['action'] . '.<br />'; 
+          $MSG3 = '<br />Zmáčknul jsi tlačítko ' . $_POST['rest'] . ', obnovil jsi 4 body výdrže.<br />' . '<br />PC použil ' . $action . '.<br />';
 }
-
 /*------------HTML---------------*/
-
 ?>
 <!DOCTYPE html>
 <!--
@@ -176,7 +159,7 @@ if (isset($_POST["rest"])){
                 </nav>
             </div>
         </header>
-        <table>
+        <table border="1">
             <tr>
                 <th></th>
                 <th>Hráč</th>
@@ -186,19 +169,19 @@ if (isset($_POST["rest"])){
             <tr>
                 <td><b>ŽIVOTY:</b></td>
                 <td><?php for ($i = 1; $i <= $_SESSION['hp1']; $i++) { echo "❤"; }  ?><br></td>
-                <th>&nbsp&nbsp&nbsp</th>
+                <th></th>
                 <td><?php for ($i = 1; $i <= $_SESSION['hp2']; $i++) { echo "❤"; }  ?><br></td>
             </tr>
             <tr>
                 <td><b>VÝDRŽ:</b></td>
                 <td><?php for ($i = 1; $i <= $_SESSION['sta1']; $i++) { echo "☯"; }  ?><br></td>
-                <th>&nbsp&nbsp&nbsp</th>
+                <th></th>
                 <td><?php for ($i = 1; $i <= $_SESSION['sta2']; $i++) { echo "☯"; }  ?><br></td>
             </tr>
             <tr>
                 <td><b>BRNĚNÍ:</b></td>
                 <td><?php for ($i = 1; $i <= $_SESSION['arm1']; $i++) { echo "♖"; }  ?><br></td>
-                <th>&nbsp&nbsp&nbsp</th>
+                <th></th>
                 <td><?php for ($i = 1; $i <= $_SESSION['arm2']; $i++) { echo "♖"; }  ?><br></td>
             </tr>
         </table>
@@ -211,18 +194,21 @@ if (isset($_POST["rest"])){
         </form>   
     </body>
 </html>
-  
 <?php
-
 /*------------VÝPIS ZPRÁV---------------*/
 
-echo $_SESSION['MSG1']; 
-echo $_SESSION['MSG2']; 
-echo $_SESSION['MSG3']; 
-
+echo $MSG1; 
+echo $MSG2; 
+echo $MSG3;
 
 /*------------VÝHRA/PROHRA---------------*/
 
-if ($_SESSION['hp1'] <= 0){echo "<script>window.location = 'defeat.php'</script>";}
-if ($_SESSION['hp2'] <= 0){echo "<script>window.location = 'victory.php'</script>";}
+if ($_SESSION['hp1'] <= 0){
+    session_unset();
+    session_destroy();
+    echo "<script>window.location = 'defeat.php'</script>";}
+if ($_SESSION['hp2'] <= 0){
+    session_unset();
+    session_destroy();
+    echo "<script>window.location = 'victory.php'</script>";}
 ?>    
